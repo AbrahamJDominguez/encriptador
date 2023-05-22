43c3cf90356e54425fdd6da3366abd1dacbf7c8d
Abraham <abrahamobish@hotmail.com>
2023-05-21 22:35:54 -0600

    Creado esqueleto de la pagina

--- js/encriptacion.js
+++ js/encriptacion.js
@@ -1,2 +1,12 @@
 const valorDesen = ['a','e','i','o','u'];
 const valorEn = ['ai','enter','imes','ober','ufat'];
+
+function estadoImg(){
+    var parrafo = document.getElementsByTagName("textarea").value;
+    if(parrafo.length>1){
+        document.getElementById("vacio").style.display = "none";
+    }
+    else{
+        document.getElementById("vacio").style.display = "";
+    }
+}
